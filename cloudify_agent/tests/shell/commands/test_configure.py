a11e65f13ef8288372e6f8351666313324913fb3
iliapolo <elip@gigaspaces.com>
2015-06-07 19:45:31 +0300

    CFY-2649 merge configure test with disable_requiretty and fix_virtualenv

--- cloudify_agent/tests/shell/commands/test_configure.py
+++ cloudify_agent/tests/shell/commands/test_configure.py
@@ -13,32 +13,16 @@
 #  * See the License for the specific language governing permissions and
 #  * limitations under the License.
 
-import mock
-
-from cloudify_agent.shell.commands import configure
-
 from cloudify_agent.tests.api.pm import only_ci
 from cloudify_agent.tests.api.pm import only_os
 
 from cloudify_agent.tests.shell.commands import BaseCommandLineTestCase
 
 
-@mock.patch('cloudify_agent.shell.commands.configure._disable_requiretty')
-@mock.patch('cloudify_agent.shell.commands.configure._fix_virtualenv')
 class TestConfigureCommandLine(BaseCommandLineTestCase):
 
-    def test_configure(self, mock_fix_virtualenv, mock_disable_requiretty):
+    @only_ci
+    @only_os('posix')
+    def test_configure(self):
         self._run('cfy-agent configure --disable-requiretty '
                   '--relocated-env')
-        mock_disable_requiretty.assert_called_once()
-        mock_fix_virtualenv.assert_called_once()
-
-    @only_ci
-    @only_os('posix')
-    def test_disable_requiretty(self):
-        configure._disable_requiretty()
-
-    @only_ci
-    @only_os('posix')
-    def test_fix_virtualenv(self):
-        configure._fix_virtualenv()
