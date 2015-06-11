f3a20fb6b13f8c2ea23c3a5beb546cf4743f6ef0
Michael Sverdlik <michael@gigaspaces.com>
2015-06-11 15:00:51 +0300

    changes

--- cloudify_agent/tests/resources/blueprints/agent-from-package/scripts/create-package.py
+++ cloudify_agent/tests/resources/blueprints/agent-from-package/scripts/create-package.py
@@ -15,42 +15,40 @@
 
 import os
 import platform
-import subprocess
 
 from agent_packager import packager
 from cloudify import ctx
 from cloudify_agent.tests.resources import get_resource
-
+from cloudify.utils import LocalCommandRunner as runner
+from cloudify.exceptions import NonRecoverableError
 
 # This should be integrated into packager
 # For now, this is the best place
 def create_windows_installer():
-    pip_req = 'https://raw.githubusercontent.com/cloudify-cosmo/' \
-              'cloudify-agent/CFY-2649-cloudify-agent/dev-requirements.txt'
     wheelhouse = get_resource('winpackage/source/wheels')
 
-    pip_cmd = 'pip wheel --wheel-dir {wheel_dir}' \
-              ' --requirement {req_file}'.format(wheel_dir=wheelhouse,
-                                                 req_file=pip_req)
-    ctx.logger.info('Building wheels into: {}'.format(wheelhouse))
-    subprocess.call(pip_cmd.split(),
-                    stderr=open(os.devnull, 'w'),
-                    stdout=open(os.devnull, 'w'))
+    pip_cmd = 'pip wheel --wheel-dir {wheel_dir} --requirement {req_file}'.\
+        format(wheel_dir=wheelhouse, req_file=config['requirements_file'])
 
-    cfy_agent_url = 'https://github.com/cloudify-cosmo/' \
-                    'cloudify-agent/archive/CFY-2649-cloudify-agent.zip'
+    ctx.logger.info('Building wheels into: {0}'.format(wheelhouse))
+    runner.run(pip_cmd.split(),
+               stderr=open(os.devnull, 'w'),
+               stdout=open(os.devnull, 'w'))
+
     pip_cmd = 'pip wheel --find-links {wheel_dir} --wheel-dir {wheel_dir} ' \
-              '{repo_url}'.format(wheel_dir=wheelhouse, repo_url=cfy_agent_url)
-    subprocess.call(pip_cmd.split(),
-                    stderr=open(os.devnull, 'w'),
-                    stdout=open(os.devnull, 'w'))
+              '{repo_url}'.format(wheel_dir=wheelhouse,
+                                  repo_url=config['cloudify_agent_module'])
+    runner.run(pip_cmd.split(),
+               stderr=open(os.devnull, 'w'),
+               stdout=open(os.devnull, 'w'))
 
-    iscc_cmd = 'iscc {}'.format(get_resource('winpackage\\create.iss'))
+    iscc_cmd = 'iscc {}'.format(get_resource(os.path.join('winpackage',
+                                                          'create.iss')))
     os.environ['VERSION'] = '0'
     os.environ['iscc_output'] = os.getcwd()
-    subprocess.call(iscc_cmd.split(),
-                    stderr=open(os.devnull, 'w'),
-                    stdout=open(os.devnull, 'w'))
+    runner.run(iscc_cmd.split(),
+               stderr=open(os.devnull, 'w'),
+               stdout=open(os.devnull, 'w'))
 
 
 config = {
@@ -71,24 +69,17 @@ try:
                         config_file=None,
                         force=False,
                         verbose=False)
+        distname, _, distid = platform.dist()
+        package_url = 'http://localhost:{0}/{1}-{2}-agent.tar.gz' \
+            .format(file_server_port, distname, distid)
     elif platform.system() == 'Windows':
         create_windows_installer()
+        package_url = 'http://localhost:{0}/cloudify_agent_0.exe' \
+            .format(file_server_port)
     else:
-        ctx.logger.error('Unsupported platform')
+        raise NonRecoverableError('Platform not supported')
 finally:
     os.chdir(original)
-
-ctx.logger.info('Constructing package_url...')
-
-if platform.system() == 'Linux':
-    distname, _, distid = platform.dist()
-    package_url = 'http://localhost:{0}/{1}-{2}-agent.tar.gz' \
-        .format(file_server_port, distname, distid)
-elif platform.system() == 'Windows':
-    package_url = 'http://localhost:{0}/cloudify_agent_0.exe' \
-        .format(file_server_port)
-else:
-    raise Exception('Unsupported platform')
 
 ctx.logger.info('Package created successfully: {0}'.format(package_url))
 ctx.logger.info('Setting runtime properties')
