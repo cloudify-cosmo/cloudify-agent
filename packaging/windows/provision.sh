6674c2d23cc7030b6e336b4900654cea1f06724a
limor-gs <limor@gigaspaces.com>
2015-08-05 16:48:59 +0300

    Add boto install to enable upload package from remote machine to s3

--- packaging/windows/provision.sh
+++ packaging/windows/provision.sh
@@ -2,6 +2,7 @@ export CORE_TAG_NAME="3.3m3"
 
 
 pip install wheel
+pip install boto==2.36.0
 
 pip wheel --wheel-dir packaging/source/wheels --requirement "https://raw.githubusercontent.com/cloudify-cosmo/cloudify-agent/$CORE_TAG_NAME/dev-requirements.txt"
 pip wheel --find-links packaging/source/wheels --wheel-dir packaging/source/wheels "https://github.com/cloudify-cosmo/cloudify-agent/archive/$CORE_TAG_NAME.zip"
