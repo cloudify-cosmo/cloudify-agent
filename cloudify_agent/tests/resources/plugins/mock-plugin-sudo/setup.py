5106805944cc57a46dd89cf7695b79c5469fb496
opencm <limor@gigaspaces.com>
2015-09-27 11:15:58 +0300

    Bump version to 3.3m6

--- cloudify_agent/tests/resources/plugins/mock-plugin-sudo/setup.py
+++ cloudify_agent/tests/resources/plugins/mock-plugin-sudo/setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='mock-plugin-sudo',
-    version='3.3a5',
+    version='3.3a6',
     author='Gigaspaces',
     author_email='cloudify@gigaspaces.com',
     packages=[
--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@
 from setuptools import setup
 
 install_requires = [
-    'cloudify-plugins-common==3.3a5',
-    'cloudify-rest-client==3.3a5',
-    'cloudify-script-plugin==1.3a5',
+    'cloudify-plugins-common==3.3a6',
+    'cloudify-rest-client==3.3a6',
+    'cloudify-script-plugin==1.3a6',
     'click==4.0',
     'celery==3.1.17',
     'jinja2==2.7.2',
@@ -29,7 +29,7 @@ install_requires = [
 
 setup(
     name='cloudify-agent',
-    version='3.3a5',
+    version='3.3a6',
     author='Gigaspaces',
     author_email='cloudify@gigaspaces.com',
     packages=[
