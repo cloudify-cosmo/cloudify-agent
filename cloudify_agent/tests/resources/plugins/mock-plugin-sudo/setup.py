fe7dec0c446b2b48dfd2249b3a1ce30b3ae516ed
opencm <limor@gigaspaces.com>
2015-11-15 11:40:18 +0200

    Bump version to 3.3

--- cloudify_agent/tests/resources/plugins/mock-plugin-sudo/setup.py
+++ cloudify_agent/tests/resources/plugins/mock-plugin-sudo/setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='mock-plugin-sudo',
-    version='3.3rc1',
+    version='3.3',
     author='Gigaspaces',
     author_email='cloudify@gigaspaces.com',
     packages=[
