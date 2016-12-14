2b24a9136b255ef69940085edf070228cdc99e2c
opencm <limor@gigaspaces.com>
2016-12-14 16:14:54 +0200

    Bump version

--- cloudify_agent/tests/resources/plugins/mock-plugin-with-requirements/setup.py
+++ cloudify_agent/tests/resources/plugins/mock-plugin-with-requirements/setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='mock-plugin-with-requirements',
-    version='4.0a9',
+    version='4.0a10',
     author='Gigaspaces',
     author_email='cloudify@gigaspaces.com',
     packages=[
