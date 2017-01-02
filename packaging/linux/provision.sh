39479ea89cd4f35bc7fa79a92460ce8e2d12c3e6
limor-gs <limor@gigaspaces.com>
2017-01-02 15:29:32 +0200

    new versioning

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -39,11 +39,9 @@ AWS_ACCESS_KEY=$4
 export REPO=$5
 
 if [ $REPO == "cloudify-versions" ];then
-    #REPO_TAG="new-versioning"
-    REPO_TAG="new-versioning"
+    REPO_TAG="master"
 else
-    #REPO_TAG=$CORE_TAG_NAME
-    REPO_TAG="new-versioning"
+    REPO_TAG=$CORE_TAG_NAME
 fi
 curl -u $GITHUB_USERNAME:$GITHUB_PASSWORD https://raw.githubusercontent.com/cloudify-cosmo/${REPO}/${REPO_TAG}/packages-urls/common_build_env.sh -o ./common_build_env.sh &&
 source common_build_env.sh &&
