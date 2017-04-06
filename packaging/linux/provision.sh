5f71249e37da38030c1b1a9065e72464ec013f7f
opencm <opencm@gigaspaces.com>
2017-04-06 13:34:02 +0300

    Update provision.sh

--- packaging/linux/provision.sh
+++ packaging/linux/provision.sh
@@ -13,7 +13,7 @@ function install_deps() {
 	elif which yum; then
 		# centos/REHL
 		sudo yum -y update &&
-		yum downgrade libyaml &&
+		sudo yum -y downgrade libyaml &&
 		sudo yum install curl python-devel make gcc gcc-c++ git libyaml-devel yum-utils libffi-devel openssl-devel -y
 	else
 		echo 'unsupported package manager, exiting'
