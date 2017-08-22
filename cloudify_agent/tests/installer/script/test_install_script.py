b8777626012e2afca6c33eaba74764b8993e0608
Pavel Brodsky <mcouthon@users.noreply.github.com>
2017-08-22 18:29:40 +0300

    CFY-7105 Don't install agent via install script in provided mode (#304)
    * CFY-7105 CFY-7168 Add test to check that add_ssl_cert function is not rendered
    when add_ssl_cert is False
    
    * CFY-7105 Don't install agent via install script in provided mode
    + tests

--- cloudify_agent/tests/installer/script/test_install_script.py
+++ cloudify_agent/tests/installer/script/test_install_script.py
@@ -37,28 +37,35 @@ class BaseInstallScriptTest(BaseTest):
 
     def setUp(self):
         super(BaseInstallScriptTest, self).setUp()
-        ctx = MockCloudifyContext(
-            node_id='d',
-            properties={'agent_config': {
-                'user': self.username,
-                'install_method': 'init_script',
-                'rest_host': 'localhost',
-                'windows': self.windows,
-                'basedir': self.temp_folder
-            }})
-        current_ctx.set(ctx)
-
-        self.addCleanup(lambda: current_ctx.clear())
+        self._set_mock_context()
+        self.addCleanup(current_ctx.clear)
         self.input_cloudify_agent = {
             'broker_ip': 'localhost',
             'ssl_cert_path': self._rest_cert_path
         }
 
-    def _run(self, *commands):
+    def _set_mock_context(self, **override_properties):
+        node_properties = {
+            'agent_config': {
+                'user': self.username,
+                'install_method': 'init_script',
+                'rest_host': 'localhost',
+                'windows': self.windows,
+                'basedir': self.temp_folder
+            }
+        }
+        node_properties['agent_config'].update(**override_properties)
+        ctx = MockCloudifyContext(node_id='d', properties=node_properties)
+        current_ctx.set(ctx)
+
+    def _get_install_script(self, add_ssl_cert=True):
         script_builder = script._get_script_builder(
             cloudify_agent=self.input_cloudify_agent
         )
-        install_script = script_builder.install_script()
+        return script_builder.install_script(add_ssl_cert=add_ssl_cert)
+
+    def _run(self, *commands):
+        install_script = self._get_install_script()
 
         # Remove last line where main function is executed
         install_script = '\n'.join(install_script.split('\n')[:-1])
@@ -142,6 +149,19 @@ class TestLinuxInstallScript(BaseInstallScriptTest):
         agent_dir = os.path.join(self.temp_folder, 'd')
         agent_ssl_cert.verify_remote_cert(agent_dir)
 
+    def test_add_ssl_func_not_rendered(self):
+        install_script = self._get_install_script(add_ssl_cert=False)
+        self.assertNotIn('add_ssl_cert', install_script)
+
+    def test_install_is_rendered_by_default(self):
+        install_script = self._get_install_script()
+        self.assertIn('install_agent', install_script)
+
+    def test_install_not_rendered_in_provided_mode(self):
+        self._set_mock_context(install_method='provided')
+        install_script = self._get_install_script()
+        self.assertNotIn('install_agent', install_script)
+
 
 @only_os('nt')
 class TestWindowsInstallScript(BaseInstallScriptTest):
@@ -165,3 +185,12 @@ class TestWindowsInstallScript(BaseInstallScriptTest):
         # basedir + node_id
         agent_dir = os.path.join(self.temp_folder, 'd')
         agent_ssl_cert.verify_remote_cert(agent_dir)
+
+    def test_add_ssl_func_not_rendered(self):
+        install_script = self._get_install_script(add_ssl_cert=False)
+        self.assertNotIn('AddSSLCert', install_script)
+
+    def test_install_not_rendered_in_provided_mode(self):
+        self._set_mock_context(install_method='provided')
+        install_script = self._get_install_script()
+        self.assertNotIn('InstallAgent', install_script)
