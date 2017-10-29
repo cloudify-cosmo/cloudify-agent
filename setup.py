0802626519cff9ee942bf33b792bfae636c5ca7a
opencm <limor@gigaspaces.com>
2017-10-29 14:24:31 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@
 from setuptools import setup
 
 install_requires = [
-    'cloudify-plugins-common==4.2.dev1',
-    'cloudify-rest-client==4.2.dev1',
+    'cloudify-plugins-common==4.2',
+    'cloudify-rest-client==4.2',
     'cloudify-script-plugin==1.5.1',
     'appdirs==1.4.3',
     'click==4.0',
@@ -35,7 +35,7 @@ install_requires = [
 
 setup(
     name='cloudify-agent',
-    version='4.2.dev1',
+    version='4.2',
     author='Gigaspaces',
     author_email='cloudify@gigaspaces.com',
     packages=[
