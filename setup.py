7352b6369deffd956fe0fbae2316d519f3a30c28
opencm <limor@cloudify.co>
2018-03-15 15:26:59 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@
 from setuptools import setup
 
 install_requires = [
-    'cloudify-plugins-common==4.3.0.1',
-    'cloudify-rest-client==4.3.0.1',
+    'cloudify-plugins-common==4.3',
+    'cloudify-rest-client==4.3',
     'cloudify-script-plugin==1.5.2',
     'appdirs==1.4.3',
     'click==4.0',
