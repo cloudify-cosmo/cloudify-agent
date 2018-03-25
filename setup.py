1a002d3a34c285b996a9da80fdb4005240983e32
idob-cloudify <idob@cloudify.co>
2018-03-25 11:44:25 +0300

    CY-153 add ctx bin dir to path

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 install_requires = [
     'cloudify-plugins-common==4.4.dev1',
     'cloudify-rest-client==4.4.dev1',
-    'cloudify-script-plugin==1.5.2',
+    'cloudify-script-plugin==1.5.3',
     'appdirs==1.4.3',
     'click==6.7',
     'celery==3.1.17',
