b9b48ffae5aa07abd96878f266c1361a8270d7e4
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2018-05-01 08:24:40 +0200

    Refactor client/worker (#374)
    * Refactor client/worker
    
    * Renames
    
    * fix1
    
    * fix2
    
    * logger..
    
    * still init
    
    * Clean
    
    * Pretty print tasks on start and on finish handling

--- cloudify_agent/worker.py
+++ cloudify_agent/worker.py
@@ -14,27 +14,17 @@
 # limitations under the License.
 ############
 
-import json
 import time
-import Queue
 import argparse
 import logging
 import logging.handlers
-from threading import Thread, Semaphore
 
-import pika
-from pika.exceptions import ConnectionClosed
 
-from cloudify import broker_config, dispatch, exceptions
+from cloudify import dispatch, exceptions
+from cloudify.amqp_client import AMQPConnection, TaskConsumer
 from cloudify.error_handling import serialize_known_exception
 from cloudify_agent.api.factory import DaemonFactory
 
-
-HEARTBEAT_INTERVAL = 30
-D_CONN_ATTEMPTS = 12
-D_RETRY_DELAY = 5
-BROKER_PORT_SSL = 5671
-BROKER_PORT_NO_SSL = 5672
 
 LOGFILE_BACKUP_COUNT = 5
 LOGFILE_SIZE_BYTES = 5 * 1024 * 1024
@@ -49,174 +39,28 @@ SUPPORTED_EXCEPTIONS = (
     exceptions.HttpException
 )
 
-
-class AMQPWorker(object):
-    MAX_BACKOFF = 30
-
-    def __init__(self, handlers, logger, name=None):
-        self._logger = logger
-        self._handlers = handlers
-        self._publish_queue = Queue.Queue()
-        self.name = name
-        self._connection_params = self._get_connection_params()
-        self._reconnect_backoff = 1
-
-    def _get_common_connection_params(self):
-        credentials = pika.credentials.PlainCredentials(
-            username=broker_config.broker_username,
-            password=broker_config.broker_password,
-        )
-        return {
-            'host': broker_config.broker_hostname,
-            'port': broker_config.broker_port,
-            'virtual_host': broker_config.broker_vhost,
-            'credentials': credentials,
-            'ssl': broker_config.broker_ssl_enabled,
-            'ssl_options': broker_config.broker_ssl_options,
-            'heartbeat': HEARTBEAT_INTERVAL
-        }
-
-    def _get_connection_params(self):
-        while True:
-            params = self._get_common_connection_params()
-            if self.name:
-                daemon = DaemonFactory().load(self.name)
-                if daemon.cluster:
-                    for node_ip in daemon.cluster:
-                        params['host'] = node_ip
-                        yield pika.ConnectionParameters(**params)
-                    continue
-            yield pika.ConnectionParameters(**params)
-
-    def _get_reconnect_backoff(self):
-        backoff = self._reconnect_backoff
-        self._reconnect_backoff = max(backoff * 2, self.MAX_BACKOFF)
-        return backoff
-
-    def _reset_reconnect_backoff(self):
-        self._reconnect_backoff = 1
-
-    def _connect(self):
-        for params in self._connection_params:
-            try:
-                self.connection = pika.BlockingConnection(params)
-            except pika.exceptions.AMQPConnectionError:
-                time.sleep(self._get_reconnect_backoff())
-            else:
-                self._reset_reconnect_backoff()
-                break
-
-        out_channel = self.connection.channel()
-        for handler in self._handlers:
-            handler.register(self.connection, self._publish_queue)
-            self._logger.info('Registered handler for {0}'
-                              .format(handler.routing_key))
-        return out_channel
-
-    def consume(self):
-        out_channel = self._connect()
-        while True:
-            try:
-                self.connection.process_data_events(0.2)
-                self._process_publish(out_channel)
-            except ConnectionClosed:
-                out_channel = self._connect()
-                continue
-
-    def _process_publish(self, channel):
-        while True:
-            try:
-                msg = self._publish_queue.get_nowait()
-            except Queue.Empty:
-                return
-            try:
-                channel.basic_publish(**msg)
-            except ConnectionClosed:
-                # if we couldn't send the message because the connection
-                # was down, requeue it to be sent again later
-                self._publish_queue.put(msg)
-                raise
-
-
-class TaskConsumer(object):
-    routing_key = ''
-
-    def __init__(self, queue, logger, threadpool_size=5):
-        self.threadpool_size = threadpool_size
-        self.exchange = queue
-        self.queue = '{0}_{1}'.format(queue, self.routing_key)
-        self._logger = logger
-        self._sem = Semaphore(threadpool_size)
-        self._output_queue = None
-
-    def register(self, connection, output_queue):
-        self._output_queue = output_queue
-
-        in_channel = connection.channel()
-        in_channel.basic_qos(prefetch_count=self.threadpool_size)
-        in_channel.exchange_declare(
-            exchange=self.exchange, auto_delete=False, durable=True)
-        in_channel.queue_declare(queue=self.queue,
-                                 durable=True,
-                                 auto_delete=False)
-        in_channel.queue_bind(queue=self.queue,
-                              exchange=self.exchange,
-                              routing_key=self.routing_key)
-        in_channel.basic_consume(self.process, self.queue)
-
-    def process(self, channel, method, properties, body):
-        try:
-            full_task = json.loads(body)
-        except ValueError:
-            self._logger.error('Error parsing task: {0}'.format(body))
-            return
-
-        self._sem.acquire()
-        new_thread = Thread(
-            target=self._process_message,
-            args=(properties, full_task)
-        )
-        new_thread.daemon = True
-        new_thread.start()
-        channel.basic_ack(method.delivery_tag)
-
-    def _process_message(self, properties, full_task):
-        try:
-            result = self.handle_task(full_task)
-        except Exception as e:
-            result = {'ok': False, 'error': repr(e)}
-            self._logger.error(
-                'ERROR - failed message processing: '
-                '{0!r}\nbody: {1}'.format(e, full_task)
-            )
-
-        if properties.reply_to:
-            self._output_queue.put({
-                'exchange': '',
-                'routing_key': properties.reply_to,
-                'properties': pika.BasicProperties(
-                    correlation_id=properties.correlation_id),
-                'body': json.dumps(result)
-            })
-        self._sem.release()
-
-    def handle_task(self, full_task):
-        raise NotImplementedError()
+logger = logging.getLogger(__name__)
 
 
 class CloudifyOperationConsumer(TaskConsumer):
     routing_key = 'operation'
     handler = dispatch.OperationHandler
 
-    def _print_task(self, task):
-        ctx = task['cloudify_task']['kwargs']['__cloudify_context']
+    def _print_task(self, ctx, action, status=None):
         if ctx['type'] == 'workflow':
-            prefix = 'Processing workflow'
+            prefix = '{0} workflow'.format(action)
             suffix = ''
         else:
-            prefix = 'Processing operation'
+            prefix = '{0} operation'.format(action)
             suffix = '\nNode ID: {0}'.format(ctx['node_id'])
-        self._logger.info(
+
+        if status:
+            suffix += '\nStatus: {0}'.format(status)
+
+        delimiter = '#' * 80
+        prefix = '\n{0}\n{1}'.format(delimiter, prefix)
+        suffix = '{0}\n{1}'.format(suffix, delimiter)
+        logger.info(
             '{prefix} on queue `{queue}` on tenant `{tenant}`:\n'
             'Task name: {name}\n'
             'Execution ID: {execution_id}\n'
@@ -232,24 +76,25 @@ class CloudifyOperationConsumer(TaskConsumer):
         )
 
     def handle_task(self, full_task):
-        self._print_task(full_task)
-        result = None
         task = full_task['cloudify_task']
         ctx = task['kwargs'].pop('__cloudify_context')
+        self._print_task(ctx, 'Started handling')
         handler = self.handler(cloudify_context=ctx, args=task.get('args', []),
                                kwargs=task['kwargs'])
         try:
             rv = handler.handle_or_dispatch_to_subprocess_if_remote()
             result = {'ok': True, 'result': rv}
-            self._logger.info('SUCCESS - result: {0}'.format(result))
+            status = 'SUCCESS - result: {0}'.format(result)
         except SUPPORTED_EXCEPTIONS as e:
             error = serialize_known_exception(e)
             result = {'ok': False, 'error': error}
-            self._logger.error(
+            status = 'ERROR - result: {0}'.format(result)
+            logger.error(
                 'ERROR - caught: {0}\n{1}'.format(
                     repr(e), error['traceback']
                 )
             )
+        self._print_task(ctx, 'Finished handling', status)
         return result
 
 
@@ -286,30 +131,18 @@ class ServiceTaskConsumer(TaskConsumer):
 
 
 def _init_logger(log_file, log_level):
-    logger = logging.getLogger(__name__)
-    handler = logging.handlers.RotatingFileHandler(
-        log_file,
-        maxBytes=LOGFILE_SIZE_BYTES,
-        backupCount=LOGFILE_BACKUP_COUNT
-    )
-    fmt = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
-    handler.setLevel(log_level)
-    handler.setFormatter(fmt)
-    logger.addHandler(handler)
-    logger.setLevel(log_level)
-    return logger
+    logging.basicConfig(level=logging.INFO)
+    return logging.getLogger(__name__)
 
 
 def make_amqp_worker(args):
-    logger = _init_logger(args.log_file, args.log_level)
+    _init_logger(args.log_file, args.log_level)
     handlers = [
-        CloudifyOperationConsumer(args.queue, logger, args.max_workers),
-        CloudifyWorkflowConsumer(args.queue, logger, args.max_workers),
-        ServiceTaskConsumer(args.queue, logger, args.max_workers),
+        CloudifyOperationConsumer(args.queue, args.max_workers),
+        CloudifyWorkflowConsumer(args.queue, args.max_workers),
+        ServiceTaskConsumer(args.queue, args.max_workers),
     ]
-    return AMQPWorker(handlers=handlers, logger=logger, name=args.name)
+    return AMQPConnection(handlers=handlers, name=args.name)
 
 
 def main():
