dfc946e015f00765bdb5969d4d4fa9092d126dd6
Pavel Brodsky <mcouthon@gmail.com>
2018-05-16 17:32:36 +0300

    CY-339 Add SystemD daemon unittest

---
+++ cloudify_agent/tests/api/pm/test_systemd.py
@@ -0,0 +1,53 @@
+#########
+# Copyright (c) 2015 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+
+import os
+import nose.tools
+from mock import patch
+
+from cloudify_agent.api.pm.systemd import SystemDDaemon
+
+from cloudify_agent.tests.api.pm import BaseDaemonProcessManagementTest
+from cloudify_agent.tests.api.pm import only_os
+from cloudify_agent.tests import get_storage_directory
+
+
+@patch('cloudify_agent.api.utils.internal.get_storage_directory',
+       get_storage_directory)
+@nose.tools.istest
+@only_os('posix')
+class TestSystemDDaemon(BaseDaemonProcessManagementTest):
+
+    @property
+    def daemon_cls(self):
+        return SystemDDaemon
+
+    def test_configure(self):
+        daemon = self.create_daemon()
+        daemon.create()
+
+        daemon.configure()
+        self.assertTrue(os.path.exists(daemon.script_path))
+        self.assertTrue(os.path.exists(daemon.config_path))
+
+    def test_delete(self):
+        daemon = self.create_daemon()
+        daemon.create()
+        daemon.configure()
+        daemon.start()
+        daemon.stop()
+        daemon.delete()
+        self.assertFalse(os.path.exists(daemon.script_path))
+        self.assertFalse(os.path.exists(daemon.config_path))
