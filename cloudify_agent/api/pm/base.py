3515698135ffa970c69cc46732216492e1ca851c
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2018-12-04 14:43:09 +0100

    CY-780 Store the heartbeat interval in broker_config.json

--- cloudify_agent/api/pm/base.py
+++ cloudify_agent/api/pm/base.py
@@ -276,7 +276,8 @@ class Daemon(object):
             'broker_password': self.broker_pass,
             'broker_hostname': self.broker_ip,
             'broker_vhost': self.broker_vhost,
-            'cluster': self.cluster
+            'cluster': self.cluster,
+            'broker_heartbeat': self.heartbeat
         }
         broker_conf_path = os.path.join(self.workdir, 'broker_config.json')
         with open(broker_conf_path, 'w') as conf_handle:
