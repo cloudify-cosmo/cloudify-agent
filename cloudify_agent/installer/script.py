157ca14e11c406f66b4cc15794d2c288998a0aa3
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2019-03-11 15:35:15 +0100

    CY-1013 Use agent's tmpdir for the installation scripts (#486) (#509)
    * Use agent's tempdir for creating tmp dirs in the runners
    
    * Use agent tempdirs in the init/download scripts
    
    * Flake
    
    * Add `if`s in the jinja templates

--- cloudify_agent/installer/script.py
+++ cloudify_agent/installer/script.py
@@ -84,7 +84,8 @@ class AgentInstallationScriptBuilder(AgentInstaller):
             install=not self.cloudify_agent.is_provided,
             configure=True,
             start=True,
-            add_ssl_cert=add_ssl_cert
+            add_ssl_cert=add_ssl_cert,
+            tmpdir=self.cloudify_agent.tmpdir
         )
 
     def _get_local_cert_content(self):
@@ -173,7 +174,8 @@ class AgentInstallationScriptBuilder(AgentInstaller):
             link=script_url,
             sudo=sudo,
             ssl_cert_content=self._get_local_cert_content(),
-            ssl_cert_path=self._get_remote_ssl_cert_path()
+            ssl_cert_path=self._get_remote_ssl_cert_path(),
+            tmpdir=self.cloudify_agent.tmpdir
         )
         if not self.cloudify_agent.is_windows:
             args_dict['user'] = self.cloudify_agent['user']
