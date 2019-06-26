7bbc22b16fe526a66b04331317df8ee9aba47abc
Isaac Shabtay <isaac@shabtay.com>
2019-06-26 12:59:35 -0700

    Use a dedicated dir owned by agent_user

--- cloudify_agent/api/pm/base.py
+++ cloudify_agent/api/pm/base.py
@@ -275,8 +275,12 @@ class Daemon(object):
         self.log_file = params.get(
             'log_file') or os.path.join(self.workdir,
                                         '{0}.log'.format(self.name))
+        # CentOS / RHEL 6 don't have /run; they have /var/run which is cleared
+        # at boot time.
+        # CentOS / RHEL 7 mount /run on tmpfs, and symlink /var/run to it.
+        # We'll use /var/run globally for the time being.
         self.pid_file = params.get(
-            'pid_file') or os.path.join('/run/{0}.pid'.format(self.name))
+            'pid_file') or '/var/run/cloudify.{0}/celery.pid'.format(self.name)
 
         # create working directory if its missing
         if not os.path.exists(self.workdir):
