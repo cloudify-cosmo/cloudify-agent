26c0c6f40c36426d9484648318f6a94971e8db91
asdf <tehasdf@gmail.com>
2020-05-06 13:19:34 +0200

    Remove the stubs

--- packaging/stub_packages/cloudify-dsl-parser/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='cloudify-dsl-parser',
-    version='5.1.0.dev1',
-    packages=[],
-    description='[DEPRECATED] A stub for the old cloudify-dsl-parser package',
-)
--- packaging/stub_packages/cloudify-plugins-common/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='cloudify-plugins-common',
-    version='5.1.0.dev1',
-    packages=[],
-    description='[DEPRECATED] A stub for the old '
-                'cloudify-plugins-common package',
-)
