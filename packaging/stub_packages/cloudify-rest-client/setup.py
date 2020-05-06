b493eaa190d37aae1020a2c41739d444fefe1b8b
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2020-05-06 13:36:52 +0200

    CY-2743 Stop shipping stub packages (#622)
    * Revert "Packaging: install git on the build images [ci skip] (#621)"
    
    This reverts commit cb7210d267b9142a10ff49349770fe8388043fc8.
    
    No need to have git if we dont have stubs
    
    * Stop shipping stub packages
    
    * Remove the stubs

--- packaging/stub_packages/cloudify-rest-client/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='cloudify-rest-client',
-    version='5.1.0.dev1',
-    packages=[],
-    description='[DEPRECATED] A stub for the old cloudify-rest-client package',
-)
--- packaging/stub_packages/cloudify-script-plugin/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='cloudify-script-plugin',
-    version='5.1.0.dev1',
-    packages=[],
-    description='[DEPRECATED] A stub for the '
-                'old cloudify-script-plugin package',
-)
