09343f6614f7d9f4a45fe7fdb72a7bf77a0b583c
opencm <limor@gigaspaces.com>
2020-10-15 09:39:30 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import sys
 from setuptools import setup
 
 install_requires = [
-    'cloudify-common==5.2.0.dev1',
+    'cloudify-common==20.12.15.dev1',
     'appdirs==1.4.3',
     'jinja2>=2.10,<2.11',
     'virtualenv==15.1.0',
@@ -35,7 +35,7 @@ else:
 
 setup(
     name='cloudify-agent',
-    version='5.2.0.dev1',
+    version='20.12.15.dev1',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=[
