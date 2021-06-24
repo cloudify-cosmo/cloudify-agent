5d51e088eb01cbd7fd72ad0b76a613d577dad425
asdf <tehasdf@gmail.com>
2021-06-24 12:55:07 +0200

    RD-2625 If cron_respawn is false, don't enable it
    Why would we always enable it?

--- cloudify_agent/api/pm/base.py
+++ cloudify_agent/api/pm/base.py
@@ -768,7 +768,8 @@ class CronRespawnDaemonMixin(Daemon):
             self.workdir, '{0}-respawn.sh'.format(self.name))
 
     def create_enable_cron_script(self):
-
+        if not self.cron_respawn:
+            return
         enable_cron_script = os.path.join(
             self.workdir, '{0}-enable-cron.sh'.format(self.name))
 
@@ -793,7 +794,8 @@ class CronRespawnDaemonMixin(Daemon):
         return enable_cron_script
 
     def create_disable_cron_script(self):
-
+        if not self.cron_respawn:
+            return
         disable_cron_script = os.path.join(
             self.workdir, '{0}-disable-cron.sh'.format(self.name))
 
