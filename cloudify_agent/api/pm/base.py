4837cc38895753ca6b1410dcf26f568b49e2efa4
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2021-06-24 14:04:21 +0200

    RD-2625 If cron_respawn is false, don't enable it (#802)
    * RD-2625 If cron_respawn is false, don't enable it
    
    Why would we always enable it?
    
    * actually, only do it in the detach agent
    
    the initd agent already has its own logic, in the bash script,
    where it calls the script or not.
    
    So, do it on the same level, in the detach agent.

--- cloudify_agent/api/pm/base.py
+++ cloudify_agent/api/pm/base.py
@@ -768,7 +768,6 @@ class CronRespawnDaemonMixin(Daemon):
             self.workdir, '{0}-respawn.sh'.format(self.name))
 
     def create_enable_cron_script(self):
-
         enable_cron_script = os.path.join(
             self.workdir, '{0}-enable-cron.sh'.format(self.name))
 
@@ -793,7 +792,6 @@ class CronRespawnDaemonMixin(Daemon):
         return enable_cron_script
 
     def create_disable_cron_script(self):
-
         disable_cron_script = os.path.join(
             self.workdir, '{0}-disable-cron.sh'.format(self.name))
 
