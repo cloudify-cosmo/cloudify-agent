973262015e83d8f69bd66ee210c77755876f7b26
Mateusz Neumann <mateumann@gmail.com>
2021-08-16 13:56:46 +0200

    RD-2992 Create rabbitmq user for agent unless it existed (#810)
    * Create rabbitmq user for agent unless it existed
    
    * Mock RabbitMQClient.get_users in operations tests
    
    * Mock get_agent_rabbitmq_user in install_local tests

--- cloudify_agent/tests/shell/commands/test_install_local.py
+++ cloudify_agent/tests/shell/commands/test_install_local.py
@@ -34,6 +34,8 @@ def test_installation_no_basedir(agent_package, agent_ssl_cert):
 
 @patch('cloudify.agent_utils.get_rest_client',
        return_value=MockRestclient())
+@patch('cloudify_agent.installer.operations.get_agent_rabbitmq_user',
+       return_value={})
 @patch('cloudify.utils.get_manager_name', return_value='cloudify')
 def _test_agent_installation(agent_ssl_cert, agent_config, *_):
     new_ctx = mock_context(agent_ssl_cert)
