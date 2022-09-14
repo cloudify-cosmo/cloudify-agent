c32c194ca7cc6d4fd497bafdbe343b8331e4ef53
Michael Glukhman <michaelg@cloudify.co>
2022-09-14 16:58:15 +0300

    RD-5919 Update CI

--- cloudify_agent/shell/commands/configure.py
+++ cloudify_agent/shell/commands/configure.py
@@ -14,7 +14,6 @@
 #  * limitations under the License.
 
 import os
-import re
 import sys
 import click
 
--- packaging/linux/linux_agent_builder.sh
+++ packaging/linux/linux_agent_builder.sh
@@ -1,12 +1,11 @@
-export AGT_NAME="$1-$2"
-export WORKDIR=$3
+export AGT_NAME="$1-$2-agent_$3"
+export WORKDIR=$(pwd)
 
-cd $WORKDIR
 mkdir -p cloudify/env
 
 # -- copy python 3.10 to "venv"
 curl https://cloudify-cicd.s3.amazonaws.com/python-build-packages/cfy-python3.10.tgz -o cfy-python3.10.tgz
-sudo tar zxvf cfy-python3.10.tgz -C /
+tar zxvf cfy-python3.10.tgz -C /
 
 cp -rf /opt/python3.10/. cloudify/env/
 cd cloudify/env/bin
@@ -17,11 +16,11 @@ ln -s pip3.10 pip3
 cd $WORKDIR
 
 # -- install agent
-export DESTINATION_TAR=centos-$AGT_NAME.tar.gz
+export DESTINATION_TAR=$AGT_NAME.tar.gz
 export PYTHONUSERBASE=$WORKDIR/cloudify/env
 cloudify/env/bin/pip install --user setuptools==65.3.0
 cloudify/env/bin/pip install --user -r dev-requirements.txt
 cloudify/env/bin/pip install --user .
 
 # -- build agent package
-tar -czf $DESTINATION_TAR cloudify/env
+tar czf $DESTINATION_TAR cloudify/env
