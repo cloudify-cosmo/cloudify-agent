576890a932ebcfd1322abef6a1b1d6ef7400772c
Mateusz Neumann <mateumann@gmail.com>
2023-01-16 19:28:07 +0100

    r/3.10/3.11/g

--- packaging/linux/linux_agent_builder.sh
+++ packaging/linux/linux_agent_builder.sh
@@ -11,19 +11,19 @@ mkdir -p cloudify/env
 # This is going to create an agent .tar.gz with a python inside the agent's
 #   env/ folder, not a true virtualenv.
 
-# -- copy python 3.10 to "venv"
-curl -f --retry 5 https://cloudify-cicd.s3.amazonaws.com/python-build-packages/cfy-python3.10-$ARCH.tgz -o cfy-python3.10.tgz
-tar --strip-components=2 -zxf cfy-python3.10.tgz -C cloudify/env/
+# -- copy python 3.11 to "venv"
+curl -f --retry 5 https://cloudify-cicd.s3.amazonaws.com/python-build-packages/cfy-python3.11-$ARCH.tgz -o cfy-python3.11.tgz
+tar --strip-components=2 -zxf cfy-python3.11.tgz -C cloudify/env/
 pushd 'cloudify/env/bin'
-    ln -s python3.10 python
-    ln -s python3.10 python3
-    ln -s pip3.10 pip
-    ln -s pip3.10 pip3
+    ln -s python3.11 python
+    ln -s python3.11 python3
+    ln -s pip3.11 pip
+    ln -s pip3.11 pip3
 popd
 
 # -- install agent
-cloudify/env/bin/python3.10 -m pip install -r dev-requirements.txt
-cloudify/env/bin/python3.10 -m pip install .
+cloudify/env/bin/python3.11 -m pip install -r dev-requirements.txt
+cloudify/env/bin/python3.11 -m pip install .
 
 # -- build agent package
 tar czf "$AGT_NAME.tar.gz" cloudify/env
