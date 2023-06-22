715f2a48a6cd49dab65df1cd32292a7ae1b162a6
opencm <limor@gigaspaces.com>
2023-06-22 06:51:36 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ install_requires = [
 
 setup(
     name='cloudify-agent',
-    version='7.0.1.dev1',
+    version='7.0.1',
     author='Cloudify',
     author_email='cosmo-admin@cloudify.co',
     packages=[
